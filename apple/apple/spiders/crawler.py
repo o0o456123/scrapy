bcd12d72279829daab867b8ded03f49a2fece282
wang jian <o0o456123@gmail.com>
2018-09-24 10:27:04 +0800

    news

---
+++ apple/apple/spiders/crawler.py
@@ -0,0 +1,24 @@
+import scrapy
+from bs4 import BeautifulSoup
+from apple.items import AppleItem
+#from scrapy.spider import CrawlSpider
+#from scrapy.linkextractors import LinkExtractor
+class AppleCrawle(scrapy.Spider):
+    name = 'apple'
+    start_urls = ['https://bbs.hupu.com/']
+    def parse(self, response):
+        domain = 'https://bbs.hupu.com/'
+        res = BeautifulSoup(response.text,'lxml')
+        for news in res.select('.textSpan'):
+            #print(news.select('span')[0].text)
+            #print(domain + news.select('a')[0]['href'])
+            yield scrapy.Request(domain + news.select('a')[0]['href'],self.parse_detail)
+
+    def parse_detail(self, response):
+        res = BeautifulSoup(response.text,'lxml')
+        appleitem = AppleItem()
+        appleitem['title'] = res.select('h1')[0].text
+        appleitem['content'] =res.select('.case')[0].text 
+        appleitem['time'] = res.select('.stime')[0].text
+        #print (res.select('h1')[0].text)
+        return appleitem
